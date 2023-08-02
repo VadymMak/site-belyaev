4aac5e73c1026bcb4164729dc580e324f178ab1c
VadymMak <makevytssvadym@gmail.com>
2023-08-02 14:29:26 +0200

    add_name

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { styled } from "styled-components";
 
 const MainPage = () => {
-  return <Container></Container>;
+  return (
+    <div>
+      <h3>Nikolay</h3>
+    </div>
+  );
 };
 
 export default MainPage;
-
-const Container = styled.div`
-  display: flex;
-  width: 320px;
-  height: 100%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  @media (min-width: 420px) {
-    width: 100%;
-    padding: 0 34px;
-  }
-`;
